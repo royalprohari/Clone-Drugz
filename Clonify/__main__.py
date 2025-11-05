c224b9ec26ce311893f25c08a058b3dde05a47d2
GhosttBatt <ghosttbatt@gmail.com>
2025-11-05 13:55:06 +0530

    __main__.py

--- Clonify/__main__.py
+++ Clonify/__main__.py
@@ -57,3 +57,13 @@ async def init():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+    if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
+    #AutoRestart
+    log("AutoRestart system started.")
+    try:
+        autorestart()
+    except KeyboardInterrupt:
+        log("AutoRestart system stopped manually.")
+    except Exception as e:
+        log(f"Unexpected error: {e}")
